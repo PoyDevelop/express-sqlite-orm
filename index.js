c1d81a9a5d3851d5048ba0334e6ac8f7d2a889ab
PoyDev <poydevelop@gmail.com>
2021-10-24 15:29:58 +0700

    add sequelize

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Product App'));
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
 
-app.listen(port, () => console.log(`Product-app listening on port ${port}!`));
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize({
+  // The `host` parameter is required for other databases
+  host: "localhost",
+  dialect: "sqlite",
+  storage: "./database.sqlite",
+});
+
+// const sequelize = new Sequelize('postgres://user:isylzjko0hk.com:5432/dbpos');
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+const Product = sequelize.define("products", {
+  name: Sequelize.STRING,
+  price: Sequelize.DOUBLE,
+});
+
+sequelize.sync({ force: true }).then(() => {
+  console.log(`Database & tables created!`);
+});
+
+var datetime = new Date();
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Product App"));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
